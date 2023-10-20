7868aa4870ec6d9a256e14348e9cea72f9d0301e
BGoel2804 <148423938+BGoel2804@users.noreply.github.com>
2023-10-20 04:47:17 +0000

    config done

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -5,6 +5,26 @@
 console.log("bhumika") 
 // code readabillity should be high
 
+console.log("Bhumika");
 
+let name = "Bhumika"
+let age = 18
+let isloggedin = false
+let state;
+
+
+// number=> 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+// null=>standalone value "empty"
+// undefined => value not assigned
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined);// undefined
+console.log(typeof dull);// object
 
 
