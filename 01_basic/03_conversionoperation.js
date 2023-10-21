44d4ead66fcc04eb23c1f5283b83faa7d8ee5b43
BGoel2804 <148423938+BGoel2804@users.noreply.github.com>
2023-10-21 09:13:09 +0000

    let chap done

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -1,26 +1,30 @@
-let score = "bhumika"
+// let score = "bhumika"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valuinNumber = Number(score)
-console.log(typeof valuinNumber);
-console.log(valuinNumber);
+// let valuinNumber = Number(score)
+// console.log(typeof valuinNumber);
+// console.log(valuinNumber);
 
-// "33"= 33
-// "33ABC"= NaN
-// true = 1; false =0
+// // "33"= 33
+// // "33ABC"= NaN
+// // true = 1; false =0
 
-let islooggedin = 0
+// let islooggedin = 0
 
-let boonleanisloggedin = Boolean(islooggedin)
-console.log(boonleanisloggedin);
-console.log(typeof boonleanisloggedin);
+// let boonleanisloggedin = Boolean(islooggedin)
+// console.log(boonleanisloggedin);
+// console.log(typeof boonleanisloggedin);
 
-// 1= true; 0 = false
-// "" = false
-// "bhumika" = true
+// // 1= true; 0 = false
+// // "" = false
+// // "bhumika" = true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// // *********************** Operations**************************
+
+let value = 3
