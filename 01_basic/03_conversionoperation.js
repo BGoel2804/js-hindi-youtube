d0635ac189d2c12511916faea823b9bb1a74a23c
BGoel2804 <148423938+BGoel2804@users.noreply.github.com>
2023-10-21 04:04:17 +0000

    conversion of datatype

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -6,3 +6,21 @@ let valuinNumber = Number(score)
 console.log(typeof valuinNumber);
 console.log(valuinNumber);
 
+// "33"= 33
+// "33ABC"= NaN
+// true = 1; false =0
+
+let islooggedin = 0
+
+let boonleanisloggedin = Boolean(islooggedin)
+console.log(boonleanisloggedin);
+console.log(typeof boonleanisloggedin);
+
+// 1= true; 0 = false
+// "" = false
+// "bhumika" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
