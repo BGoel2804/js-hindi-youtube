3fa7b0658601a1d44ea8f8a06cabdb44a9132e93
BGoel2804 <148423938+BGoel2804@users.noreply.github.com>
2023-10-27 06:18:34 +0000

    operation done

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -28,3 +28,39 @@
 // // *********************** Operations**************************
 
 let value = 3
+let negValue = -value
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "bhumika"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// //console.log(3 + 4 * 5 % 3); NG
+
+console.log(+true);
+console.log(+"");
+//console.log(true+); error 
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
