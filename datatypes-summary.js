5d26e13062bdc8ffae66904f896560bbba4b6627
BGoel2804 <148423938+BGoel2804@users.noreply.github.com>
2023-11-02 05:32:27 +0000

    data type under process

---
+++ datatypes-summary.js
@@ -0,0 +1,7 @@
+// primitive : call by value
+
+// 7 types : String, Number, Boolean(yes or no), null(empty),undefined(value defined not inputed it yet), Symbol, BigInt(scientific value or too big value)
+
+// Reference Type(Non Primitive)
+
+// Array, Objects,
