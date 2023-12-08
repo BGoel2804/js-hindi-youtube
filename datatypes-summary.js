17455e6de7e606a9d8336c74840ed554a8cb8767
BGoel2804 <148423938+BGoel2804@users.noreply.github.com>
2023-12-08 04:55:25 +0000

    data type summary

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -1,7 +1,40 @@
 // primitive : call by value
 
-// 7 types : String, Number, Boolean(yes or no), null(empty),undefined(value defined not inputed it yet), Symbol, BigInt(scientific value or too big value)
+// 7 types : String, Number, Boolean(yes or no), null(empty),undefined(value defined not inputed it yet), Symbol(to make value unique), BigInt(scientific value or too big value)
+
+
+const score = 100 
+const scoreValue = 100.3
+
+const isloggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 3456876456986n
+
+
+
+
+
 
 // Reference Type(Non Primitive)
 
-// Array, Objects,
+// Array[], Objects{},Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "bhumika",
+    age: 26,
+}
+
+const myFunction = function(){
+    console.log(helloworld)
+}
+
+console.log(typeof anotherId)
